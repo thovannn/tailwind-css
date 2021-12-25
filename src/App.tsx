cd9864cfef659b68a0a0ee7bcc4a124034fa5b2d
thovannn <thaovan5501@gmail.com>
2021-12-25 16:09:59 +0700

    push respository

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GradientText } from "./components/GradientText";
+
+function App() {
+  return (
+    <div className="App">
+      <GradientText text={"React with Tailwind CSS and TypeScript"} />
+    </div>
+  );
+}
+
+export default App;
